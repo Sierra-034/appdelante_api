6df349b0e0782dadcf245a40c5403e0264221037
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-08-08 23:24:53 -0500

    Introducción 1-12: Usamos a Joi para validar el body de request a /productos

--- api/recursos/productos/productos.routes.js
+++ api/recursos/productos/productos.routes.js
@@ -1,62 +1,66 @@
 const express = require('express');
 const productos = require('../../../database').productos;
 const uuidv4 = require('uuid/v4');
+const Joi = require('joi');
 
 const productosRouter = express.Router();
 
-productosRouter.get('/', (request, response) => {
-    response.json(productos);
+const blueprintProducto = Joi.object().keys({
+    titulo: Joi.string().max(100).required(),
+    precio: Joi.number().positive().precision(2).required(),
+    moneda: Joi.string().length(3).uppercase()
 });
 
-productosRouter.post('/', (request, response) => {
-    let nuevoProducto = request.body;
-    if (!nuevoProducto.titulo || !nuevoProducto.precio || !nuevoProducto.moneda) {
-        response.status(400).send('Tu producto debe especificar un título, precio y moneda');
+const validarProducto = (request, response, next) => {
+    const validationResult = Joi.validate(request.body, blueprintProducto, { abortEarly: false, convert: false });
+    if (validationResult.error !== null) {
+        
+        const validationErrors = validationResult.error.details.reduce((accumulator, error) => {
+            return accumulator += `[${error.message}]\n`;
+        }, "\n\t");
+
+        response.status(400).send(`Tu producto en el body debe especificar un título, precio y moneda: ${validationErrors}`);
         return;
     }
 
+    next();
+};
+
+const validateProductExistance = (request, response, next) => {
+    const indexToLook = productos.findIndex(producto => producto.id === request.params.id);
+    if (indexToLook === -1) {
+        response.status(404).send(`El producto con id [${request.params.id}] no fué encontrado`);
+        return;
+    }
+
+    next();
+}
+
+productosRouter.get('/', (request, response) => response.json(productos));
+
+productosRouter.post('/', validarProducto, (request, response) => {
+    let nuevoProducto = request.body;
     nuevoProducto.id = uuidv4();
     productos.push(nuevoProducto);
     response.status(201).json(nuevoProducto);
 });
 
-productosRouter.get('/:id', (request, response) => {
-    const id = request.params.id;
-    const indexToResponse = productos.findIndex(producto => producto.id === id);
-    if (indexToResponse === -1) {
-        response.status(404).send(`El producto con id [${request.params.id}] no existe`);
-        return;
-    }
-
-    const productToResponse = productos[indexToResponse];
-    response.status(200).json(productToResponse);
+productosRouter.get('/:id', validateProductExistance, (request, response) => {
+    const product = productos.find(producto => producto.id === request.params.id);
+    response.status(200).json(product);
 });
 
-productosRouter.put('/:id', (request, response) => {
+productosRouter.put('/:id', [validateProductExistance, validarProducto], (request, response) => {
+    let productReplacer = request.body;
     const idToReplace = productos.findIndex(producto => producto.id === request.params.id);
-    if (idToReplace === -1) {
-        response.status(404).send(`El producto con id [${request.params.id}] no existe`);
-        return;
-    }
-
-    const productReplacer = request.body;
-    if (!productReplacer.titulo || !productReplacer.precio || !productReplacer.moneda) {
-        response.status(400).send('Tu producto debe especificar un título, precio y moneda');
-        return;
-    }
 
     productReplacer.id = request.params.id;
     productos[idToReplace] = productReplacer;
     response.status(200).json(productReplacer);
 });
 
-productosRouter.delete('/:id', (request, response) => {
+productosRouter.delete('/:id', validateProductExistance, (request, response) => {
     const idToDelete = productos.findIndex(producto => producto.id === request.params.id);
-    if (idToDelete === -1) {
-        response.status(404).send(`El producto con id [${request.params.id}] no existe. Nada que`);
-        return;
-    }
-
     const deletedProduct = productos.splice(idToDelete, 1);
     response.status(200).json(deletedProduct);
 });
