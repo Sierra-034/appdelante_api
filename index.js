18d5cb3729b7b9cb589fd835688be99367aafb56
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-01 02:41:06 -0500

    Configuración y uso de un logger 2-1: El valor de un logger en un sistema de producción

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const productosRouter = require('./api/recursos/productos/productos.routes');
+const winston = require('winston');
+
+const incluirFecha = winston.format((info) => {
+    info.message = `${new Date().toISOString()} ${info.message}`;
+    return info;
+})
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            level: 'info',
+            handleExceptions: true,
+            format: winston.format.combine(
+                incluirFecha(),
+                winston.format.simple()
+            ),
+            maxsize: 5120000,   // 5 Mb
+            maxFiles: 5,
+            filename: `${__dirname}/logs-de-aplicacion.log`
+        })
+    ]
+})
+
+logger.info(__dirname);
+logger.info('Hola, soy logger', {compania: 'appdelante'});
+logger.error('Algo explotó');
+logger.warn('Algo inesperado ocurrió');
+logger.debug('Llamada de debug');
 
 const app = express();
 app.use(bodyParser.json());
