100c1e5d1a421deafdccdb9b0f4db03bbb79d65a
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-01 21:22:53 -0500

    2-3: Integrar morgan con winston para Loggear Data de Request

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productosRouter = require('./api/recursos/productos/productos.routes');
 const logger = require('./utils/logger');
+const morgan = require('morgan');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(morgan('short', {
+    stream: {
+        write: message => logger.info(message)
+    }
+}));
+
 app.use('/productos', productosRouter);
 
 app.get('/', (request, response) => {
@@ -14,3 +21,4 @@ app.get('/', (request, response) => {
 app.listen(3000, () => {
     logger.info('Escuchando en el puerto 3000.');
 });
+ 
