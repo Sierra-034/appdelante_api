dd095c95be40e7e3af1b34673abfef4f0bb1a5ab
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-05 23:42:16 -0500

    3-6: Testing del registro de usuarios con postman

--- api/recursos/usuarios/usuarios.routes.js
+++ api/recursos/usuarios/usuarios.routes.js
@@ -12,7 +12,7 @@ usuariosRouter.get('/', (request, response) => {
     response.json(usuarios);
 });
 
-usuariosRouter.post('/', (request, response) => {
+usuariosRouter.post('/', validarUsuario, (request, response) => {
     let nuevoUsuario = request.body;
     let indice = usuarios.findIndex(usuario => {
         return usuario.username === nuevoUsuario.username || usuario.email === nuevoUsuario.email;
@@ -21,20 +21,25 @@ usuariosRouter.post('/', (request, response) => {
     if (indice !== -1) {
         logger.info('Email o username ya existen en la base de datos.');
         response.status(409).send('El email o username ya están asociados a una cuenta.');
+        return;
     }
 
     bcrypt.hash(nuevoUsuario.password, 10, (err, hashedPassword) => {
         if (err) {
             logger.info('Ocurrió algo al tratar de obtener el hash de una contraseña.', err);
             response.status(500).send('Ocurrió un error procesando creación de usuario.');
+            return;
         }
 
         usuarios.push({
             username: nuevoUsuario.username,
-            password: hashedPassword,
-            email: nuevoUsuario.email
+            email: nuevoUsuario.email,
+            password: hashedPassword
         })
 
         response.status(200).send('Usuario creado exitosamente.');
+        return;
     });
 });
+
+module.exports = usuariosRouter;
--- api/recursos/usuarios/usuarios.validate.js
+++ api/recursos/usuarios/usuarios.validate.js
@@ -9,9 +9,12 @@ const blueprintUsuario = Joi.object({
 
 module.exports = (request, response, next) => {
     const resultado = blueprintUsuario.validate(request.body, { abortEarly: false, convert: false });
-    if (resultado.error === null) next();
+    if (resultado.error === undefined) {
+        next();
+        return;
+    }
     
-    logger.info('Producto falló la validación', resultado.error.details);
+    logger.info('Usuario falló la validación', resultado.error.details.map(error => error.message));
     response.status(400).send("Información del usuario no cumple con los requisitos. El nombre de usuario debe ser alfanumérico y tener entre 3 y 30 caracteres. La contraseña debe tener entre 6 y 200 caracteres. Asegúrate de que el email sea válido");
     return;
 }
