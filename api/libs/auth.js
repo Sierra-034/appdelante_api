0858f30766a4dfa5b36e9412f0520aa9ba856b72
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-13 23:29:53 -0500

    3-12: Crear estrategia de autenticación con el módulo passport-jwt

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -1,30 +1,23 @@
 const logger = require('./../../utils/logger');
 const usuarios = require('./../../database').usuarios;
 const bcrypt = require('bcrypt');
+const passportJWT = require('passport-jwt');
 
-module.exports = (username, password, done) => {
-    // Buscar al usuario en la base de datos.
-    // Si no existe, llamar a done con falso e indicar que el usuario no pudo ser autenticado en el logger
-    // Obtener el hashed password desde el usuario de la base de datos.
-    // Comparar el password.
-        // Si coincide, entonces llamar a done con true e indicarlo en el logger
-        // De lo contrario, entonces llamar a done con false e indicarlo en el logger
-
-    let index = usuarios.findIndex(usuario => usuario.username === username);
-    if (index === -1) {
-        logger.info(`Usuario ${username} no existe. No puede ser autenticado.`);
-        done(null, false);
-        return;
-    }
-
-    const hashedPassword = usuarios[index].password;
-    bcrypt.compare(password, hashedPassword, (err, iguales) => {
-        if (iguales) {
-            logger.info(`Usuario ${username} completó la autenticación.`);
-            done(null, true);
-        } else {
-            logger.info(`El usuario ${username} no completó la autenticación. Contraseña incorrecta`);
-            done(null, false);
-        }
-    })
+const jwtOptions = {
+    secretOrKey: 'esto es un secreto',
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
 }
+
+module.exports = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
+    const index = usuarios.findIndex(usuario => usuario.id === jwtPayload.id);
+    if (index === -1) {
+        logger.info(`JWT token no es válido. Usuario con id ${jwtPayload.id} no existe`);
+        next(null, false);
+    } else {
+        logger.info(`Usuario ${usuarios[index].username} suministró un token válido. Autenticación completada.`);
+        next(null, {
+            username: usuarios[index].username,
+            id: usuarios[index].id
+        });
+    }
+});
