7fb30e312e595b508344f92125f9df86e2fedce8
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-09-13 23:12:33 -0500

    3-10: Validación de pedidos de Login con Joi

--- api/recursos/usuarios/usuarios.routes.js
+++ api/recursos/usuarios/usuarios.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const logger = require('../../../utils/logger');
-const validarUsuario = require('./usuarios.validate');
+const validarUsuario = require('./usuarios.validate').validarUsuario;
+const validarPedidoLogin = require('./usuarios.validate').validarPedidoLogin;
 const usuarios = require('../../../database').usuarios;
 
 const usuariosRouter = express.Router();
@@ -14,9 +16,10 @@ usuariosRouter.get('/', (request, response) => {
 
 usuariosRouter.post('/', validarUsuario, (request, response) => {
     let nuevoUsuario = request.body;
-    let indice = usuarios.findIndex(usuario => {
-        return usuario.username === nuevoUsuario.username || usuario.email === nuevoUsuario.email;
-    });
+    let indice = usuarios.findIndex(usuario => (
+        usuario.username === nuevoUsuario.username ||
+        usuario.email === nuevoUsuario.email
+    ));
 
     if (indice !== -1) {
         logger.info('Email o username ya existen en la base de datos.');
@@ -34,10 +37,35 @@ usuariosRouter.post('/', validarUsuario, (request, response) => {
         usuarios.push({
             username: nuevoUsuario.username,
             email: nuevoUsuario.email,
-            password: hashedPassword
+            password: hashedPassword,
+            id: uuidv4()
         })
 
         response.status(200).send('Usuario creado exitosamente.');
+        return;
+    });
+});
+
+usuariosRouter.post('/login', validarPedidoLogin, (request, response) => {
+    const usuarioNoAutenticado = request.body;
+    const index = usuarios.findIndex(usuario => usuarioNoAutenticado.username === usuario.username);
+    if (index === -1) {
+        logger.info(`Usuario ${usuarioNoAutenticado.username} no existe. No puede ser autenticado.`);
+        response.status(400).send('Credenciales incorrectas. El usuario no existe.');
+        return;
+    }
+
+    const hashedPassword = usuarios[index].password;
+    bcrypt.compare(usuarioNoAutenticado.password, hashedPassword, (err, iguales) => {
+        if (iguales) {
+            const token = jwt.sign({ id: usuarios[index].id }, 'esto es un secreto', { expiresIn: 86400 });
+            logger.info(`Usuario ${usuarioNoAutenticado.username} completó la autenticación exitósamente.`);
+            response.status(200).json({ token });
+        } else {
+            logger.info(`Usuario ${usuarioNoAutenticado.username} no completó la autenticación. Contraseña incorrecta.`);
+            response.status(400).send('Credenciales incorrectas. Asegúrate de que el usuario y contraseña sean correctas.');
+        }
+
         return;
     });
 });
--- api/recursos/usuarios/usuarios.validate.js
+++ api/recursos/usuarios/usuarios.validate.js
@@ -7,14 +7,44 @@ const blueprintUsuario = Joi.object({
     email: Joi.string().email().required()
 });
 
-module.exports = (request, response, next) => {
+const validarUsuario = (request, response, next) => {
     const resultado = blueprintUsuario.validate(request.body, { abortEarly: false, convert: false });
     if (resultado.error === undefined) {
         next();
         return;
     }
-    
+
     logger.info('Usuario falló la validación', resultado.error.details.map(error => error.message));
-    response.status(400).send("Información del usuario no cumple con los requisitos. El nombre de usuario debe ser alfanumérico y tener entre 3 y 30 caracteres. La contraseña debe tener entre 6 y 200 caracteres. Asegúrate de que el email sea válido");
+    response.status(400).send(
+        "Información del usuario no cumple con los requisitos. " +
+        "El nombre de usuario debe ser alfanumérico y tener entre 3 y 30 caracteres. " +
+        "La contraseña debe tener entre 6 y 200 caracteres. " +
+        "Asegúrate de que el email sea válido"
+    );
     return;
 }
+
+const blueprintPedidoLogin = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required()
+});
+
+const validarPedidoLogin = (request, response, next) => {
+    const resultado = blueprintPedidoLogin.validate(request.body, { abortEarly: false, convert: false });
+    if (resultado.error === undefined) {
+        next();
+        return;
+    }
+
+    logger.info('Usuario falló la validación', resultado.error.details.map(error => error.message));
+    response.status(400).send(
+        'Login falló. Debes especificar el username y la contraseña del usuario. ' +
+        'Ambos deben ser strings.'
+    );
+    return;
+};
+
+module.exports = {
+    validarUsuario,
+    validarPedidoLogin
+};
