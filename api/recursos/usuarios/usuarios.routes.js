c8cdd8e38cf8be0491e76b24dd682e380823c473
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-01-18 11:19:10 -0600

    3-9: Crear ruta de login para generar tokens de autenticación

--- api/recursos/usuarios/usuarios.routes.js
+++ api/recursos/usuarios/usuarios.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const logger = require('../../../utils/logger');
 const validarUsuario = require('./usuarios.validate');
@@ -34,10 +35,33 @@ usuariosRouter.post('/', validarUsuario, (request, response) => {
             username: nuevoUsuario.username,
             email: nuevoUsuario.email,
             password: hashedPassword,
+            id: uuidv4(),
         });
 
         response.status(201).send('Usuario creado exitosamente');
     });
+});
+
+usuariosRouter.post('/login', (request, response) => {
+    const usuarioNoAutenticado = request.body;
+    const index = usuarios.findIndex(usuario => usuario.username === usuarioNoAutenticado.username);
+    if (index === -1) {
+        logger.info(`Usuario ${usuarioNoAutenticado.username} no existe. No pudo ser autenticado`);
+        response.status(400).send('Credenciales incorrectas. El usuario no existe');
+        return;
+    }
+
+    const hashedPassword = usuarios[index].password;
+    bcrypt.compare(usuarioNoAutenticado.password, hashedPassword, (error, iguales) => {
+        if (iguales) {
+            const token = jwt.sign({id: usuarios[index].id}, 'esto es un secreto', {expiresIn: 86400});
+            logger.info(`Usuario ${usuarioNoAutenticado.username} completó autenticación existósamente.`);
+            response.status(200).json({ token })
+        } else {
+            logger.info(`Usuario ${usuarioNoAutenticado.username} no completó autentiación. Contraseña incorrecta`);
+            response.status(400).send('Credenciales incorrectas. Asegúrate que el username y contraseñas sean correctas.');
+        }
+    })
 });
 
 module.exports = usuariosRouter;
