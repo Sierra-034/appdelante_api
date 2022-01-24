b93dfe70b53795ae9f2618f65e30b6788a57263e
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-01-24 13:52:21 -0600

    3-12: Crear estrategia de autenticación con el módulo passport-jwt

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -1,23 +1,24 @@
 const logger = require('../../utils/logger');
 const usuarios = require('../../database').usuarios;
 const bcrypt = require('bcrypt');
+const passportJWT = require('passport-jwt');
 
-module.exports = (username, password, done) => {
-    const index = usuarios.findIndex(usuario => usuario.username === username);
+const jwtOptions = {
+    secretOrKey: 'esto es un secreto',
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+}
+
+module.exports = new passportJWT.Strategy(jwtOptions, (jwt_payload, done) => {
+    const index = usuarios.findIndex(usuario => usuario.id === jwt_payload.id);
     if (index === -1) {
-        logger.info(`Usuario ${username} no existe. No pudo ser autenticado.`);
+        logger.info(`JWT no es válido. Usuario con id ${jwt_payload.id} no existe.`);
         done(null, false);
         return;
     }
 
-    const hashedPassword = usuarios[index].password;
-    bcrypt.compare(password, hashedPassword, (err, iguales) => {
-        if (iguales) {
-            logger.info(`Usuario ${username} completó la autenticación.`)
-            done(null, true);
-        } else {
-            logger.info(`Usuario ${username} no completó la autenticación. Contraseña incorrecta.`);
-            done(null, false);
-        }
-    })
-}
+    logger.info(`Usuario ${usuarios[index].username} suministró token válido. Autenticación completada.`);
+    done(null, {
+        username: usuarios[index].username,
+        id: usuarios[index].id,
+    });
+});
