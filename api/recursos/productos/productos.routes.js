19e688b5cacbbf8b15808017f53534af3682d083
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-10-26 21:33:57 -0500

    6-4: Configurar express Para que utiliza Middleware de Errores.

--- api/recursos/productos/productos.routes.js
+++ api/recursos/productos/productos.routes.js
@@ -8,64 +8,53 @@ const procesarErrores = require('../../libs/errorHandler').procesarErrores;
 const jwtAuthenticate = passport.authenticate('jwt', { session: false });
 const productosRouter = express.Router();
 
-productosRouter.get('/', (request, response) => {
-    productosController.obtenerProductos()
-        .then(productos => {
-            response.json(productos);
-        })
-        .catch(err => {
-            response.status(500).send("Error al leer de la base de datos");
-        });
-});
+productosRouter.get('/', procesarErrores((request, response) => {
+    return productosController.obtenerProductos()
+        .then(productos => response.json(productos));
+}));
 
 productosRouter.post(
     '/',
     [jwtAuthenticate, validators.validateProduct],
     procesarErrores((request, response) => {
 
-        return productosController.crearProducto(request.body, request.user.username)
+        return productosController
+            .crearProducto(request.body, request.user.username)
             .then(producto => {
-                logger.info("Producto agregado a la colección productos", producto.toObject());
+                logger.info(
+                    "Producto agregado a la colección productos",
+                    producto.toObject()
+                );
                 response.status(201).json(producto);
-            })
+            });
     }));
 
-productosRouter.get('/:id', validators.validateId, (request, response) => {
-    let id = request.params.id;
-    productosController.obtenerProducto(id)
-        .then(producto => {
-            if (!producto) {
-                response.status(404).send(`Producto con id [${id}] no existe`);
-            } else {
-                response.status(200).json(producto);
-            }
-        })
-        .catch(err => {
-            logger.error(`Excepción ocurrió al tratar de obtener producto con id [${id}]`, err);
-            response.status(500).send(`Error ocurrió obteniendo producto con id [${id}]`);
-        });
-});
+productosRouter.get(
+    '/:id',
+    validators.validateId,
+    procesarErrores((request, response) => {
+
+        let id = request.params.id;
+        return productosController.obtenerProducto(id)
+            .then(producto => {
+                if (!producto) {
+                    response.status(404)
+                        .send(`Producto con id [${id}] no existe`);
+                } else {
+                    response.status(200).json(producto);
+                }
+            });
+    }));
 
 productosRouter.put(
     "/:id",
     [jwtAuthenticate, validators.validateProduct],
-    async (request, response) => {
+    procesarErrores(async (request, response) => {
+
         let id = request.params.id;
         let requestUsuario = request.user.username;
         let productoReemplazar;
-
-        try {
-            productoReemplazar = await productosController.obtenerProducto(id);
-        } catch (error) {
-            logger.warn(
-                `Excepción ocurrió al procesar la modificación del producto con id [${id}]`,
-                error
-            );
-            response
-                .status(500)
-                .send(`Error ocurrió modificando producto con id [${id}]`);
-            return;
-        }
+        productoReemplazar = await productosController.obtenerProducto(id);
 
         if (!productoReemplazar) {
             response.status(404).send(`El producto con id [${id}] no existe`);
@@ -74,12 +63,15 @@ productosRouter.put(
 
         if (productoReemplazar.dueño !== requestUsuario) {
             logger.warn(
-                `Usuario [${requestUsuario}] no es dueño de producto con id [${id}]. Dueño real es [${productoReemplazar.dueño}]. Request no será procesado`
+                `Usuario [${requestUsuario}] no es dueño de \
+            producto con id [${id}]. Dueño real es \
+            [${productoReemplazar.dueño}]. Request no será procesado`
             );
             response
                 .status(401)
                 .send(
-                    `No eres dueño del producto con id [${id}]. Solo puedes modificar productos creados por tí.`
+                    `No eres dueño del producto con id [${id}]. Solo \
+                puedes modificar productos creados por tí.`
                 );
             return;
         }
@@ -93,52 +85,35 @@ productosRouter.put(
                     producto.toObject()
                 );
             })
-            .catch((err) => {
-                logger.error(
-                    `Excepción al tratar de reemplazar producto con id [${id}]`,
-                    err
-                );
-                response
-                    .status(500)
-                    .send(`Error ocurrió reemplazando producto con id [${id}]`);
-            });
-    }
-);
+    }));
 
-productosRouter.delete('/:id', [jwtAuthenticate, validators.validateId], async (request, response) => {
-    let id = request.params.id;
-    let productoBorrar;
+productosRouter.delete(
+    '/:id',
+    [jwtAuthenticate, validators.validateId],
+    procesarErrores(async (request, response) => {
 
-    // async / await
-    try {
+        let id = request.params.id;
+        let productoBorrar;
+
+        // async / await
         productoBorrar = await productosController.obtenerProducto(id);
-    } catch (error) {
-        logger.error(`Excepción ocurrió al procesar el borrado de producto con id [${id}]`, error);
-        response.status(500).send(`Error ocurrió borrando producto con id [${id}]`);
-        return
-    }
 
-    if (!productoBorrar) {
-        logger.info(`Producto con id [${id}] no existe. Nada que borrar.`);
-        response.status(404).send(`Producto con id [${id}] no existe. Nada que borrar.`);
-        return
-    }
+        if (!productoBorrar) {
+            logger.info(`Producto con id [${id}] no existe. Nada que borrar.`);
+            response.status(404).send(`Producto con id [${id}] no existe. Nada que borrar.`);
+            return
+        }
 
-    let usuarioAutenticado = request.user.username;
-    if (productoBorrar.dueño !== usuarioAutenticado) {
-        logger.info(`Usuario [${usuarioAutenticado}] no es dueño de producto con id [${id}]. Dueño real es [${productoBorrar.dueño}]. Request no será procesado`);
-        response.status(401).send(`No eres dueño del producto con id [${id}]. Solo puedes borrar productos creados por tí.`);
-        return
-    }
+        let usuarioAutenticado = request.user.username;
+        if (productoBorrar.dueño !== usuarioAutenticado) {
+            logger.info(`Usuario [${usuarioAutenticado}] no es dueño de producto con id [${id}]. Dueño real es [${productoBorrar.dueño}]. Request no será procesado`);
+            response.status(401).send(`No eres dueño del producto con id [${id}]. Solo puedes borrar productos creados por tí.`);
+            return
+        }
 
-    try {
         let productoBorrado = await productosController.borrarProducto(id);
         logger.info(`Producto con id [${id}] fué borrado`);
         response.json(productoBorrado);
-    } catch (error) {
-        logger.error(`Error ocurrió borrando el producto con id [${id}]`, error);
-        response.status(500).send(`Error ocurrió borrando producto con id [${id}]`);
-    }
-});
+    }));
 
 module.exports = productosRouter;
--- api/recursos/usuarios/usuarios.routes.js
+++ api/recursos/usuarios/usuarios.routes.js
@@ -7,149 +7,120 @@ const validarUsuario = require('./usuarios.validate').validarUsuario;
 const validarLogin = require('./usuarios.validate').validarLogin;
 const config = require('../../../config');
 const usuarioController = require("./usuarios.controller");
+const procesarErrores = require('../../libs/errorHandler').procesarErrores;
+
 
 const usuariosRouter = express.Router();
+
+class DatosDeUsuarioYaEnUso extends Error {
+    constructor(message) {
+        super(message);
+        this.message = message ||
+            'El email o usuario ya están asociados.';
+        this.status = 409;
+        this.name = 'DatosDeUsuarioYaEnUso';
+    }
+}
 
 function transformarBodyALowercase(request, response, next) {
     request.body.username &&
         (request.body.username = request.body.username.toLowerCase());
-    request.body.email && (request.body.email = request.body.email.toLowerCase());
+    request.body.email &&
+        (request.body.email = request.body.email.toLowerCase());
     next();
 }
 
-usuariosRouter.get("/", (request, response) => {
-    usuarioController
+usuariosRouter.get("/", procesarErrores((request, response) => {
+    return usuarioController
         .obtenerUsuarios()
         .then((usuarios) => {
             response.json(usuarios);
         })
-        .catch((err) => {
-            logger.error(`Error al obtener todos los usuarios`, err);
-            response.sendStatus(500);
-        });
-});
+}));
 
 usuariosRouter.post(
     "/",
     [validarUsuario, transformarBodyALowercase],
-    (request, response) => {
-        const nuevoUsuario = request.body;
+    procesarErrores((request, response) => {
 
-        usuarioController
+        const nuevoUsuario = request.body;
+        return usuarioController
             .usuarioExiste(nuevoUsuario.username, nuevoUsuario.email)
             .then((usuarioExiste) => {
                 if (usuarioExiste) {
                     logger.warn(
-                        `Email [${nuevoUsuario.email}] o username [${nuevoUsuario.username}] ya existe en la base de datos`
+                        `Email [${nuevoUsuario.email}] o username \
+					[${nuevoUsuario.username}] ya existe en la base \
+					de datos`
                     );
-                    response
-                        .status(409)
-                        .send(`El email o usuario ya están asociados con una cuenta`);
-                    return;
+                    throw new DatosDeUsuarioYaEnUso();
                 }
 
-                bcrypt.hash(nuevoUsuario.password, 10, (err, hashedPassword) => {
-                    if (err) {
-                        logger.error(
-                            `Error ocurrió al tratar de obtener el hash de una contraseña`,
-                            err
-                        );
-                        response.status(500).send(`Error procesando creación del usuario.`);
-                        return;
-                    }
-
-                    usuarioController
-                        .crearUsuario(nuevoUsuario, hashedPassword)
-                        .then((nuevoUsuario) => {
-                            response.status(201).send(`Usuario creado exitósamente.`);
-                        })
-                        .catch((err) => {
-                            logger.error(
-                                `Error ocurrió al tratar de crear nuevo usuario`,
-                                err
-                            );
-                            response
-                                .status(500)
-                                .send(`Error ocurrió al tratar de crear nuevo usuario`);
-                        });
-                });
+                return bcrypt.hash(nuevoUsuario.password, 10);
             })
-            .catch((err) => {
-                logger.error(
-                    `Error ocurrió al tratar de verificar si usuario [${nuevoUsuario.username}] con email [${nuevoUsuario.email}] ya existe.`
-                );
-                response
-                    .status(500)
-                    .send(`Error ocurrió al tratar de crear nuevo usuario.`);
+            .then(hash => {
+                return usuarioController
+                    .crearUsuario(nuevoUsuario, hash)
+                    .then((nuevoUsuario) => response
+                        .status(201)
+                        .send(`Usuario creado exitósamente.`));
             });
-    });
+    }));
 
 usuariosRouter.post(
     "/login",
     [validarLogin, transformarBodyALowercase],
-    async (request, response) => {
+    procesarErrores(async (request, response) => {
+
         const usuarioNoAutenticado = request.body;
         let usuarioRegistrado;
-
-        try {
-            usuarioRegistrado = await usuarioController.obtenerUsuario({
-                username: usuarioNoAutenticado.username,
-            });
-        } catch (error) {
-            logger.error(
-                `Error ocurrió al tratar de determinar si el usuario [${usuarioNoAutenticado.username}] ya existe`,
-                err
-            );
-            response.status(500).send(`Error ocurrió duarnte el proceso de login.`);
-            return;
-        }
+        usuarioRegistrado = await usuarioController.obtenerUsuario({
+            username: usuarioNoAutenticado.username,
+        });
 
         if (!usuarioRegistrado) {
             logger.info(
-                `Usuario [${usuarioNoAutenticado.username}] no existe. No pudo ser autenticado`
+                `Usuario [${usuarioNoAutenticado.username}] no existe. No \
+			pudo ser autenticado`
             );
             response
                 .status(400)
                 .send(
-                    `Credenciales incorrectas. Asegúrate que el username y contraseña sean correctos.`
+                    `Credenciales incorrectas. Asegúrate que el username \
+				y contraseña sean correctos.`
                 );
             return;
         }
 
         let contraseñaCorrecta;
-        try {
-            contraseñaCorrecta = await bcrypt.compare(
-                usuarioNoAutenticado.password,
-                usuarioRegistrado.password
-            );
-        } catch (error) {
-            logger.error(
-                `Error ocurrió al tratar de verificar si la contraseña es correcta`
-            );
-            response.status(500).send(`Error ocurrió durante el proceso de login`);
-            return;
-        }
+        contraseñaCorrecta = await bcrypt.compare(
+            usuarioNoAutenticado.password,
+            usuarioRegistrado.password
+        );
 
         if (contraseñaCorrecta) {
-            const token = jwt.sign({ id: usuarioRegistrado.id }, config.jwt.secreto, {
-                expiresIn: config.jwt.tiempoDeExpiracion,
-            });
+            const token = jwt.sign(
+                { id: usuarioRegistrado.id },
+                config.jwt.secreto,
+                { expiresIn: config.jwt.tiempoDeExpiracion }
+            );
             logger.info(
-                `Usuario ${usuarioNoAutenticado.username} completó autenticación existósamente.`
+                `Usuario ${usuarioNoAutenticado.username} completó \
+			autenticación existósamente.`
             );
             response.status(200).json({ token });
         } else {
             logger.info(
-                `Usuario ${usuarioNoAutenticado.username} no completó autentiación. Contraseña incorrecta`
+                `Usuario ${usuarioNoAutenticado.username} no completó \
+			autentiación. Contraseña incorrecta.`
             );
             response
                 .status(400)
                 .send(
-                    "Credenciales incorrectas. Asegúrate que el username y contraseñas sean correctas."
+                    "Credenciales incorrectas. Asegúrate que el username \
+				y contraseñas sean correctas."
                 );
         }
-
-    }
-);
+    }));
 
 module.exports = usuariosRouter;
