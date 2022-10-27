af8909df1eb16d7351ee7e06a6946bf707f6b52a
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-10-27 18:05:46 -0500

    7-3: Terminar Tests a GET /usuarios

--- api/recursos/usuarios/usuarios.routes.test.js
+++ api/recursos/usuarios/usuarios.routes.test.js
@@ -4,6 +4,24 @@ let Usuario = require('./usuarios.model');
 let app = require('../../../index').app;
 let server = require('../../../index').server;
 
+let dummyUsuarios = [
+    {
+        username: 'samuel',
+        email: 'samuel@gmail.com',
+        password: 'holaquetal',
+    },
+    {
+        username: 'pablo',
+        email: 'pablo@gmail.com',
+        password: 'quepaso',
+    },
+    {
+        username: 'diego',
+        email: 'diego@gmail.com',
+        password: 'nomedigas',
+    },
+];
+
 describe('Usuarios', () => {
 
     beforeEach((done) => {
@@ -25,5 +43,21 @@ describe('Usuarios', () => {
                     done();
                 });
         });
+
+        test('Si existen usuarios, debería retornarlos en un array', (done) => {
+            Promise.all(dummyUsuarios.map(usuario => (new Usuario(usuario)).save()))
+                .then(usuarios => {
+                    request(app)
+                        .get('/usuarios')
+                        .end((error, res) => {
+                            expect(res.status).toBe(200);
+                            expect(res.body).toBeInstanceOf(Array);
+                            expect(res.body).toHaveLength(3);
+                            done();
+                        })
+                })
+        })
+
     });
+
 });
