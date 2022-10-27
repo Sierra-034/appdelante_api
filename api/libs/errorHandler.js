6d8e9e0bb72c063ebf35e988cc9771e0c49465a1
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-10-26 20:06:41 -0500

    6-3: Middlewares de express para manejo de errores

--- api/libs/errorHandler.js
+++ api/libs/errorHandler.js
@@ -1,8 +1,33 @@
+const mongoose = require('mongoose');
 const logger = require("../../utils/logger");
 
 exports.procesarErrores = (fn) => {
-    return function(req, res, next) {
-        fn(req, res, next)
-            .catch(error => logger.error("Dentro de procesar errores", error))
+    return function(request, response, next) {
+        fn(request, response, next).catch(next);
     }
 };
+
+exports.procesarErroresDeDB = (error, request, response, next) => {
+    if (error instanceof mongoose.Error || error.name === 'MongoError') {
+        logger.error(`Ocurrió un error relacionado a mongoose.`, error);
+        error.message = "Error relacionado a la base de datos ocurrió inesperadamente. Para ayuda contacte a sierra@gmail.com";
+        error.status = 500;
+    }
+
+    next(error);
+};
+
+exports.prodErrors = (error, request, response, next) => {
+    response.status(error.status || 500);
+    response.send({
+        message: error.message
+    });
+}
+
+exports.devErrors = (error, request, response, next) => {
+    response.status(error.status || 500);
+    response.send({
+        message: error.message,
+        stack: error.stack || '',
+    });
+}
